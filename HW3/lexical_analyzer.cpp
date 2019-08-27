bf1709b1edc2a7eae43456cda425c3569ec6b408
Carlos Estrada <carlos.deg02@hotmail.com>
2019-08-27 07:24:16 -0500

    Added hw3

---
+++ HW3/lexical_analyzer.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+#include <string>
+
+int main(int argc, char *argv[]))
+{
+    string input;
+    while (true)
+    {
+        scanf("%s", &input)
+        // TODO: Process the input with the transition matrix
+        // TODO: Display the resulting token to the user
+    }
+
+    return 0;
+}
